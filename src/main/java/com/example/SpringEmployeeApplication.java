f37bfd20243ef65e3454056ab74190e6e594a1ba
AKHIL JADAV <52011634+JADAVAKHIL@users.noreply.github.com>
2021-06-01 18:12:04 +0530

    Update SpringEmployeeApplication.java

--- src/main/java/com/example/SpringEmployeeApplication.java
+++ src/main/java/com/example/SpringEmployeeApplication.java
@@ -9,7 +9,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class SpringEmployeeApplication {
+public class SpringEmployeeApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context=SpringApplication.run(SpringEmployeeApplication.class, args);

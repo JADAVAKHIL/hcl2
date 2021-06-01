c776e19643f7e0420b0b1f78b629ba062066be67
AKHIL JADAV <52011634+JADAVAKHIL@users.noreply.github.com>
2021-06-01 17:43:16 +0530

    Update SpringEmployeeApplication.java

--- src/main/java/com/example/SpringEmployeeApplication.java
+++ src/main/java/com/example/SpringEmployeeApplication.java
@@ -3,6 +3,10 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+ 
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class SpringEmployeeApplication {
@@ -11,5 +15,12 @@ public class SpringEmployeeApplication {
 		ConfigurableApplicationContext context=SpringApplication.run(SpringEmployeeApplication.class, args);
 		
 	}
+@Override 
+protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+return application.sources(SpringEmployeeApplication.class);
+
+ 
+}
+
 
 }
